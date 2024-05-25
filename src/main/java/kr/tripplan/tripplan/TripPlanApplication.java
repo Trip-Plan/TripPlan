15d19c58020b65c8f3ab3d653ebaabab4485e300
cksgud0403 <cksgud0403@naver.com>
2024-05-25 19:07:58 +0900

    Initial commit

---
+++ src/main/java/kr/tripplan/tripplan/TripPlanApplication.java
@@ -0,0 +1,13 @@
+package kr.tripplan.tripplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripPlanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/tripplan/tripplan/TripPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.tripplan.tripplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
